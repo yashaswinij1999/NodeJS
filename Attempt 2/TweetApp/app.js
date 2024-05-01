676b585d611f5f25c2d6c1cac708c7de06b2aacf
Yashaswini J <yashaswinij1999>
2024-05-01 14:26:24 +0530

    added

--- Attempt 2/TweetApp/app.js
+++ Attempt 2/TweetApp/app.js
@@ -1,35 +1,47 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuidv4 } = require("uuid");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(express.urlencoded({ extended: true }));
 
 let comments = [
   {
-    id: 1,
+    id: uuidv4(),
     username: "Todd",
     comment: "Lol that is so funny",
   },
   {
-    id: 2,
+    id: uuidv4(),
     username: "Skyler",
     comment: "I like to go birdwatching with my dog",
   },
   {
-    id: 3,
+    id: uuidv4(),
     username: "Sk8erBoi",
     comment: "Plz delete your account,Todd",
   },
   {
-    id: 4,
+    id: uuidv4(),
     username: "onlysayswoof",
     comment: "woof woof woof",
   },
 ];
 
+app.post("/comments", (req, res) => {
+  const { username, comment } = req.body;
+  comments.push({ username, comment, id: uuidv4() });
+  res.redirect("/comments");
+});
+
+app.get("/comments/new", (req, resp) => {
+  resp.render("new");
+});
+
 app.get("/comments", (req, resp) => {
-  resp.render("index", { comments });
+  resp.render("index", { ...comments });
 });
 
 app.listen(3000, () => {
