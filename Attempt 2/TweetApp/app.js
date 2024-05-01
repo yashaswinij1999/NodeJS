587a1751fbaedf1140d1475d3b694812afd4ea0e
Yashaswini J <yashaswinij1999>
2024-05-01 16:37:52 +0530

    edit comment

--- Attempt 2/TweetApp/app.js
+++ Attempt 2/TweetApp/app.js
@@ -3,10 +3,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
 
 let comments = [
   {
@@ -31,9 +33,16 @@ let comments = [
   },
 ];
 
-app.get("/comments/edit/:id", (req, res) => {
+app.patch("/comments/:id", (req, res) => {
   const { id } = req.params;
-  res.render('/edit',{i})
+  const newComment = req.body.comment;
+  console.log(id, newComment);
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const obj = comments.find((el) => el.id === id);
+  res.render("edit", { obj });
 });
 
 app.get("/comments/show/:id", (req, res) => {
